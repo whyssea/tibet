08f0cd4787c1898cda8479e3f1a268f6f6a49a78
seyelich <robot11148@gmail.com>
2024-07-21 14:58:25 +0700

    chore: add eslint & prettier

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  pluginPrettier,
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+];
